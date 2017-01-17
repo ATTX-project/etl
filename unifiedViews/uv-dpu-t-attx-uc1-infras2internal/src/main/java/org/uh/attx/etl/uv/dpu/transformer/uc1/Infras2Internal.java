8bb548f294a4f5fd74569577adcbbf5275edd84a
jkesanie <jkesanie@lm5-hulib-1.hulib.helsinki.fi>
2017-01-17 14:18:01 +0200

    Changed implementation to reflect the changes to the UC1 datamodel

--- unifiedViews/uv-dpu-t-attx-uc1-infras2internal/src/main/java/org/uh/attx/etl/uv/dpu/transformer/uc1/Infras2Internal.java
+++ unifiedViews/uv-dpu-t-attx-uc1-infras2internal/src/main/java/org/uh/attx/etl/uv/dpu/transformer/uc1/Infras2Internal.java
@@ -1,8 +1,6 @@
 package org.uh.attx.etl.uv.dpu.transformer.uc1;
 
-import com.vaadin.server.communication.JSONSerializer;
 import eu.unifiedviews.dataunit.DataUnit;
-import eu.unifiedviews.dataunit.DataUnitException;
 import eu.unifiedviews.dataunit.files.FilesDataUnit;
 import eu.unifiedviews.dataunit.files.WritableFilesDataUnit;
 import eu.unifiedviews.dpu.DPU;
@@ -14,33 +12,24 @@ import org.slf4j.LoggerFactory;
 import eu.unifiedviews.helpers.dpu.config.ConfigHistory;
 import eu.unifiedviews.helpers.dpu.context.ContextUtils;
 import eu.unifiedviews.helpers.dpu.exec.AbstractDpu;
-import info.aduna.io.IOUtil;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Set;
-import java.util.logging.Level;
-import jdk.nashorn.internal.objects.NativeArray;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.openrdf.model.Graph;
-import org.openrdf.model.Model;
 import org.openrdf.model.Resource;
-import org.openrdf.model.Statement;
 import org.openrdf.model.ValueFactory;
 
 import org.openrdf.model.impl.LinkedHashModel;
-import org.openrdf.model.impl.ValueFactoryBase;
 import org.openrdf.model.impl.ValueFactoryImpl;
 import org.openrdf.model.vocabulary.DCTERMS;
 import org.openrdf.model.vocabulary.RDF;
-import org.openrdf.model.vocabulary.RDFS;
 import org.openrdf.rio.RDFFormat;
-import org.openrdf.rio.RDFHandlerException;
 import org.openrdf.rio.Rio;
-import sun.misc.IOUtils;
 
 /**
  * Main data processing unit class.
@@ -69,6 +58,8 @@ public class Infras2Internal extends AbstractDpu<Infras2InternalConfig_V1> {
     protected void innerExecute() throws DPUException {
         ContextUtils.sendShortInfo(ctx, "t-attx-uc1-infras2internal.message");
         
+        long pipelineID = ctx.getExecMasterContext().getDpuContext().getPipelineId();
+        
         // model to be serialized
         
         Graph g = new LinkedHashModel();
@@ -81,7 +72,7 @@ public class Infras2Internal extends AbstractDpu<Infras2InternalConfig_V1> {
             FilesDataUnit.Entry outputEntry = FilesHelper.createFile(output, "infras.ttl");
             FileOutputStream out = new FileOutputStream(FilesHelper.asFile(outputEntry));
             
-            Resource type = f.createURI(config.getBaseName() + "types/infrastructure");
+            Resource type = f.createURI(config.getBaseName() + "types/Infrastructure");
 
 
             // get the input files
@@ -89,24 +80,24 @@ public class Infras2Internal extends AbstractDpu<Infras2InternalConfig_V1> {
             for(FilesDataUnit.Entry entry : files) {
                 File file = FilesHelper.asFile(entry);
                 // JSON file
-                String c = IOUtil.readString(file);
+                String c = new String(Files.readAllBytes(Paths.get(file.toURI())));
                 JSONArray infras = new JSONArray(c);
                 for(int i = 0; i < infras.length(); i++) {
                     JSONObject infra = infras.getJSONObject(i);
                     
                     // Using couple of basic properties
                     // urn = base for ID
-                    String nameFI = infra.getString("name_FI");
+                    String nameEN = infra.getString("name_EN");
                     String urn = infra.getString("urn");
                     // add statements to the model
                     
                     
-                    Resource r = f.createURI(config.getBaseName() + "infra/" + URLEncoder.encode(urn));
+                    Resource r = f.createURI(config.getBaseName() + pipelineID + "/work/infra/" + URLEncoder.encode(urn));
                     
                     
                     g.add(f.createStatement(r, RDF.TYPE, type));
-                    g.add(f.createStatement(r, DCTERMS.IDENTIFIER, f.createLiteral(urn)));
-                    g.add(f.createStatement(r, DCTERMS.TITLE, f.createLiteral(nameFI)));
+                    g.add(f.createStatement(r, DCTERMS.IDENTIFIER, f.createURI(urn)));
+                    g.add(f.createStatement(r, DCTERMS.TITLE, f.createLiteral(nameEN)));
                     
                 }
                 
