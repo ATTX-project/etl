75c1b1d30489b1c55a12379b8e40e22f8507de9d
Stefan Negru <stefan.negru@helsinki.fi>
2017-02-13 10:04:59 +0200

    WF-API postponing unit test

--- wf-API/tests/test_app.py
+++ wf-API/tests/test_app.py
@@ -1,11 +1,11 @@
-import falcon
+# import falcon
 import unittest
-import httpretty
+# import httpretty
 # import requests
 from falcon import testing
 from wf_api.app import create
-from wf_api.app import api_version
-from datetime import datetime
+# from wf_api.app import api_version
+# from datetime import datetime
 # from wf_api.utils.db import connect_DB
 
 
@@ -30,13 +30,13 @@ class TestApp(appTest):
     #     response = self.app.get('/')
     #     assert(response.status_code == 404)
 
-    @httpretty.activate
-    def test_activity_ok(self):
-        """Test GET map response is OK."""
-        httpretty.register_uri(httpretty.GET, "http://localhost:4301/0.1/activity?modifiedSince=2017-02-03T08%3A14%3A14Z", status=304)
-        params = {"modifiedSince": str(datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"))}
-        result = self.simulate_get('/{0}/activity'.format(api_version), params=params)
-        assert(result.status == falcon.HTTP_304)
+    # @httpretty.activate
+    # def test_activity_ok(self):
+    #     """Test GET map response is OK."""
+    #     httpretty.register_uri(httpretty.GET, "http://localhost:4301/0.1/activity?modifiedSince=2017-02-03T08%3A14%3A14Z", status=304)
+    #     params = {"modifiedSince": str(datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"))}
+    #     result = self.simulate_get('/{0}/activity'.format(api_version), params=params)
+    #     assert(result.status == falcon.HTTP_304)
 
 
 if __name__ == "__main__":
